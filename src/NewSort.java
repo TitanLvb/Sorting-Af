7abf18af1f930c5029e218c4afc1a6608fbaeb2c
shubham <shubham@localhost>
2022-09-09 11:47:41 +0530

    New Sort

---
+++ src/NewSort.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+public class NewSort {
+
+	public NewSort() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int[] arr = { 13, 74, 69, 45, 21, 9, 101, 102 };
+        Arrays.sort(arr);
+        System.out.printf("Modified arr[] : %s",
+                          Arrays.toString(arr));
+
+	}
+
+}
